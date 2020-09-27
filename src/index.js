aae1b07a593b619519ba5105ee3954198353b1a7
Brian McMillen <brianmcmillen1@gmail.com>
2020-09-27 14:11:23 -0400

    add logs for package keys

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 const cache = require('@actions/cache');
 const core = require('@actions/core');
 const github = require('@actions/github');
-const md5File = require('md5-file');
 
 async function main() {
-  try {
-    const paths = ['~/.npm'];
-    const preKey = 'node-modules-';
-    const hash = md5File.sync('package-lock.json');
-    const key = preKey + hash;
-    const cacheKey = await cache.restoreCache(paths, key, [preKey]);
-    console.log({ cacheKey });
+  const paths = ['~/.npm'];
+  const preKey = 'node-modules-';
+  const key = preKey + 'hash';
+  const cacheKey = await cache.restoreCache(paths, key, [preKey]);
+  console.log({ cacheKey });
 
-    if (cacheKey !== key) {
-      const cacheId = await cache.saveCache(paths, key);
-      console.log({ cacheId });
-    }
-  } catch (error) {
-    core.setFailed(error.message)
+  if (cacheKey !== key) {
+    const cacheId = await cache.saveCache(paths, key);
+    console.log({ cacheId });
   }
+
+  console.log('cache keys', Object.keys(cache));
+  console.log('core keys', Object.keys(core));
+  console.log('github keys', Object.keys(github));
 };
 
-main();
+try {
+  main();
+} catch (error) {
+  core.setFailed(error.message);
+}
