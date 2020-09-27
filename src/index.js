aced25361bec2e6b90a7ef67cb5c2d8ef87ecea7
Brian McMillen <brianmcmillen1@gmail.com>
2020-09-27 13:57:10 -0400

    hash package-lock.json to use in key

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const cache = require('@actions/cache');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const md5File = require('md5-file');
 
 async function main() {
   try {
     const paths = ['~/.npm'];
-    const preKey = 'os-node-modules-';
-    const key = preKey + '${{ hashFiles("package-lock.json") }}';
+    const preKey = 'node-modules-';
+    const hash = md5File.sync('package-lock.json');
+    const key = preKey + hash;
     const cacheKey = await cache.restoreCache(paths, key, [preKey]);
-      // ["${{ runner.os }}-node-modules-"]
     console.log({ cacheKey });
 
     if (cacheKey !== key) {
