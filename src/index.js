e1e4a4e3cf4f86cbe3a7e8b31eb6a35d80985f30
Brian McMillen <brianmcmillen1@gmail.com>
2020-09-27 13:02:49 -0400

    add caching

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+const cache = require('@actions/cache');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const cacheKey = await cache.restoreCache(
+    ['~/.npm'],
+    "${{ runner.os }}-node-modules-${{ hashFiles('package-lock.json') }}",
+    ["${{ runner.os }}-node-modules-"]
+  );
+  console.log({ cacheKey });
+
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`)
 } catch (error) {
