c92f735be43063085189cb2c9357e319c02da9ee
Brian McMillen <mcmillenb@users.noreply.github.com>
2020-10-13 12:02:13 +0000

    build before release

--- dist/index.js
+++ dist/index.js
@@ -57500,13 +57500,14 @@ async function npmConfig() {
 }
 
 async function publish() {
-  const { eventName: name, payload: e, ref, head } = github.context.github || {};
+  const { eventName: name, payload: e, ref, head } = github.context || {};
+  log({ name, payload: e, ref, head });
+
   await gitConfig(head.ref);
   await npmConfig();
   const merged = e && e.pull_request && e.pull_request.merged === 'true';
   const pushOrMerge = (name === 'push' || merged);
-
-  log({ name, merged, ref, head, pushOrMerge });
+  log({ merged, pushOrMerge });
 
   let version;
   if (name === 'pull_request') {
@@ -57516,8 +57517,8 @@ async function publish() {
   } else if (ref === 'refs/heads/master' && pushOrMerge) {
     version = 'minor';
   }
-
   log({ version });
+
   if (version) {
     await exec(`npx lerna version ${version} -y && npx lerna publish from-git -y`);
   }
